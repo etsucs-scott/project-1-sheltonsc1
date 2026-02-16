6ac3d23a370304603ad0c6b20623c8fb65f38f53
Samuel Shelton <SHELTONSC1@ETSU.EDU>
2026-02-16 17:37:37 -0500

    added maze methods and player methods to program.cs

--- AdventureGame/AdventureGame.Cli/Program.cs
+++ AdventureGame/AdventureGame.Cli/Program.cs
@@ -5,15 +5,29 @@ namespace AdventureGame.Cli;
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static void Main(string[] args, Maze maze)
     {
         // introduce the game and its objective to the player
         Console.WriteLine("Welcome to the Adventure Game!");
         Console.WriteLine("You are a brave adventurer exploring a mysterious dungeon.");
         Console.WriteLine("Your goal is to defeat the monsters and escape.");
         Console.WriteLine("Good luck!");
+
+        maze.InitializeGrid();
+        maze.GenerateMaze(0, 0);
+        maze.SetEndTile();
+        maze.PlaceItems(1, 1);
+        maze.PlaceMonster(2, 2);
+        maze.PrintPlayer(0, 0);
+        maze.IsWalkable(0, 1);
+
         // Create a player with 150 health
         Player player = new Player(150);
+        player.Move("right", maze);
+        player.Move("left", maze);
+        player.Move("down", maze);
+        player.Move("up", maze);
+
         // Create some monsters
         Monster goblin = new Monster("Slime", 50);
         // Create some items
